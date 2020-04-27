00cd034490454e499418c92c5711752e889831da
oliwiabrozek <oliwiabrozek@gmail.com>
2020-04-27 20:26:36 +0200

    Dodanie awatara, ikon telefonu, wiadomości i maila

--- src/components/details/DetailsComponent.tsx
+++ src/components/details/DetailsComponent.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
-import {Button, StyleSheet, Text, View} from 'react-native';
+import {Button, Image, StyleSheet, Text, View} from 'react-native';
 import {IconButton, Colors} from 'react-native-paper';
+import {Linking} from 'react-native'
+import {Divider} from 'react-native-elements';
+import {Avatar} from 'react-native-elements';
 
 interface Props {
     id: number,
@@ -11,25 +14,81 @@ const DetailsComponent = (props: Props) => {
     const {
         id,
         onEdit,
-    } = props;
 
+    } = props;
     return (
-      <View>
-          <Text>Imię i nazwisko kontaktu o id: {id}</Text>
+      <View style={styles.all}>
+          <Avatar
+            size="xlarge"
+            icon={{name: 'user', type: 'font-awesome'}}
+            containerStyle={{flex: 2, marginLeft: 20, marginTop: 100, marginRight: 20, marginBottom: 100}}
+            onPress={() => console.log("Works!")}
+            showEditButton
+          />
+          <View>
+
+              <Text style={styles.name}>Zdzisław Sroczyński</Text>
+          </View>
+          <Divider style={{backgroundColor: 'blue'}}/>
+
 
           <View style={styles.container}>
+
               <IconButton style={styles.button}
                           icon="account-heart"
                           color={Colors.blue500}
-                          size={100}
+                          size={50}
                           onPress={() => console.log('<3')}
               />
               <IconButton style={styles.button}
                           icon="account-edit"
                           color={Colors.blue500}
-                          size={100}
-                          onPress={() => {onEdit(id)}}
+                          size={50}
+                          onPress={() => {
+                              onEdit(id)
+                          }}
               />
+              <IconButton style={styles.button}
+                          icon="delete"
+                          color={Colors.blue500}
+                          size={50}
+                          onPress={() => console.log('delete')}
+              />
+              <IconButton
+                          icon="account-group"
+                          color={Colors.blue500}
+                          size={50}
+                          onPress={() => console.log('account-group')}
+              />
+          </View>
+
+          <Divider style={{backgroundColor: 'blue'}}/>
+          <View>
+              <View style={styles.phone}>
+                  <Text>+48 123 456 789</Text>
+                  <IconButton
+                              icon="phone"
+                              color={Colors.blue500}
+                              size={25}
+                              onPress={() => console.log('call')}
+                  />
+                  <IconButton
+                              icon="message"
+                              color={Colors.blue500}
+                              size={25}
+                              onPress={() => console.log('m')}
+                  />
+              </View>
+
+              <View style={styles.mail}>
+                  <Text>samograjepolsl@gmail.com</Text>
+                  <IconButton style={styles.button}
+                              icon="email"
+                              color={Colors.blue500}
+                              size={25}
+                              onPress={() => console.log('delete')}
+                  />
+              </View>
           </View>
 
       </View>
@@ -37,13 +96,25 @@ const DetailsComponent = (props: Props) => {
 };
 
 const styles = StyleSheet.create({
+    all: {
+        flexDirection:'column'
+    },
     container: {
         flex: 1,
         flexDirection: 'row',
         justifyContent: 'space-between'
     },
     button: {
-        width: '50%',
+        width: '20%',
+    },
+    name: {
+        fontSize: 24
+    },
+    phone: {
+        flexDirection: 'row',
+    },
+    mail: {
+        flexDirection: 'row',
     }
 });
 
