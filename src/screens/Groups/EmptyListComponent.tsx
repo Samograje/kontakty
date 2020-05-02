8dfab553df483577c18d52a23ecb304ca4775720
Kamil Rusin <lotrusek@gmail.com>
2020-05-02 17:48:06 +0200

    Dodaj obrazek w EmptyListComponent

--- src/screens/Groups/EmptyListComponent.tsx
+++ src/screens/Groups/EmptyListComponent.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
+import {MaterialCommunityIcons} from 'react-native-vector-icons';
 
 const EmptyListComponent = () => {
     return (
         <View style={styles.informationContainer}>
+            <MaterialCommunityIcons size={100}
+                                    name={'account-group'}
+            />
             <Text style={styles.message}>No groups available. Add one!</Text>
         </View>
     );
