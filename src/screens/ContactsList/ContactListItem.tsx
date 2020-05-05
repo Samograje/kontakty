877d1eb103c88b321b29c36ef08233a7ec3fc1e5
Bartłomiej Szlachta <bartlomiej.szlachta@softiq.pl>
2020-05-05 23:25:18 +0200

    Klikalność elementów listy kontaktów

--- src/screens/ContactsList/ContactListItem.tsx
+++ src/screens/ContactsList/ContactListItem.tsx
@@ -1,28 +1,39 @@
 import React, { ReactElement } from 'react';
 import { StyleSheet, Text } from 'react-native';
 import { Contact } from '../../redux/reducers/ContactsReducer';
+import { TouchableRipple } from 'react-native-paper';
 
 interface Props {
     item: Contact;
+    onClick: (contact: Contact) => void,
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        padding: 10,
+        paddingLeft: 20,
+    },
+    text: {
         fontSize: 20,
-        margin: 10,
-        marginLeft: 20,
     },
 });
 
 const ContactListItem = (props: Props): ReactElement => {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const { firstName, lastName, photoUrl } = props.item;
+    const {
+        firstName,
+        lastName,
+        photoUrl,
+    } = props.item;
+
+    const onPress = () => props.onClick(props.item);
 
     return (
-        <Text style={styles.container}>
-            {firstName} {lastName}
-        </Text>
+        <TouchableRipple style={styles.container} onPress={onPress}>
+            <Text style={styles.text}>
+                {firstName} {lastName}
+            </Text>
+        </TouchableRipple>
     );
 };
 
--- src/screens/ContactsList/ContactsList.tsx
+++ src/screens/ContactsList/ContactsList.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Button, SectionList, StyleSheet, View } from 'react-native';
+import React, { ReactElement } from 'react';
+import { Button, SectionList, SectionListRenderItem, SectionListRenderItemInfo, StyleSheet, View } from 'react-native';
 import { Contact } from '../../redux/reducers/ContactsReducer';
 import ContactListItem from './ContactListItem';
 import ContactsListSectionHeader from './ContactsListSectionHeader';
@@ -7,7 +7,7 @@ import ContactsListEmptyBanner from './ContactsListEmptyBanner';
 
 interface Props {
     onCreate: () => void;
-    onView: (id: number) => void;
+    onView: (id: number | null) => void;
     onExample: () => void;
     data: {
         title: string;
@@ -22,19 +22,26 @@ const styles = StyleSheet.create({
 });
 
 const ContactsList = (props: Props): JSX.Element => {
-    const { onCreate, onView, onExample, data } = props;
+    const {
+        onCreate,
+        onView,
+        onExample,
+        data,
+    } = props;
 
     const keyExtractor = (item, index): string => item + index;
+    const renderItem: SectionListRenderItem<Contact> = (props: SectionListRenderItemInfo<Contact>): ReactElement => (
+        <ContactListItem item={props.item} onClick={() => onView(props.item.id)} />
+    );
 
     return (
         <View style={styles.container}>
-            <Button title='Szczegóły kontaktu o id 4' onPress={(): void => onView(4)} />
             <Button title='FAB dodaj nowy kontakt' onPress={onCreate} />
             <Button title='Click here!' onPress={onExample} />
             <SectionList
                 sections={data}
                 keyExtractor={keyExtractor}
-                renderItem={ContactListItem}
+                renderItem={renderItem}
                 renderSectionHeader={ContactsListSectionHeader}
                 ListEmptyComponent={ContactsListEmptyBanner}
             />
