e7946a3b1d087bbfda24518b30c382d697afeb21
Bartłomiej Szlachta <bartlomiej.szlachta@softiq.pl>
2020-05-06 11:37:49 +0200

    Dodanie FAB

--- src/screens/ContactsList/ContactsList.tsx
+++ src/screens/ContactsList/ContactsList.tsx
@@ -1,5 +1,6 @@
 import React, { ReactElement } from 'react';
 import { Button, SectionList, SectionListRenderItem, SectionListRenderItemInfo, StyleSheet, View } from 'react-native';
+import { FAB } from 'react-native-paper';
 import { Contact } from '../../redux/reducers/ContactsReducer';
 import ContactListItem from './ContactListItem';
 import ContactsListSectionHeader from './ContactsListSectionHeader';
@@ -19,6 +20,14 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
     },
+    fixedView: {
+        position: 'absolute',
+        right: 30,
+        bottom: 30,
+    },
+    fab: {
+        backgroundColor: 'darkgreen',
+    },
 });
 
 const ContactsList = (props: Props): JSX.Element => {
@@ -37,7 +46,6 @@ const ContactsList = (props: Props): JSX.Element => {
 
     return (
         <View style={styles.container}>
-            <Button title='FAB dodaj nowy kontakt' onPress={onCreate} />
             <Button title='Click here!' onPress={onExample} />
             <SectionList
                 sections={data}
@@ -46,6 +54,14 @@ const ContactsList = (props: Props): JSX.Element => {
                 renderSectionHeader={ContactsListSectionHeader}
                 ListEmptyComponent={ContactsListEmptyBanner}
             />
+            <View style={styles.fixedView}>
+                {/* eslint-disable-next-line prettier/prettier */}
+                <FAB
+                    style={styles.fab}
+                    icon="plus"
+                    onPress={onCreate}
+                />
+            </View>
         </View>
     );
 };
