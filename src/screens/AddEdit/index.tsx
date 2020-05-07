1402899430a5597ea405e7031c1f1656d765d712
Kamil Rusin <lotrusek@gmail.com>
2020-05-07 20:46:47 +0200

    Dodaj sprawdzenie permissions

--- src/screens/AddEdit/index.tsx
+++ src/screens/AddEdit/index.tsx
@@ -1,30 +1,32 @@
 /* eslint-disable prettier/prettier */
 import React, { useCallback, useState } from 'react';
-import AddEdit from "./AddEdit";
-import { getContacts } from "../../redux/selectors/Selectors";
+import AddEdit from './AddEdit';
+import { getContacts } from '../../redux/selectors/Selectors';
 import { useNavigation } from '@react-navigation/native';
 import { useDispatch, useSelector } from 'react-redux';
 import * as ImagePicker from 'expo-image-picker';
-import { formLabels, modes } from "../StringsHelper";
-import {createContact, updateContact} from "../../redux/actions/ActionCreators";
-import {contactT} from "../CustomTypes";
+import { formLabels, modes } from '../StringsHelper';
+import { createContact, updateContact } from '../../redux/actions/ActionCreators';
+import { contactT } from '../CustomTypes';
+import { Alert } from 'react-native';
+import { getCameraPermissionsAsync, getCameraRollPermissionsAsync } from 'expo-image-picker';
 
-const AddEditScreen  = ({route, navigation}): JSX.Element => {
+const AddEditScreen = ({ route, navigation }): JSX.Element => {
     //wartości początkowe
-    const {navigate} = useNavigation();
-    const {id, mode} = route.params;
+    const { navigate } = useNavigation();
+    const { id, mode } = route.params;
     const contacts = useSelector(getContacts);
     const dispatch = useDispatch();
     const isEdit = mode === modes.edit;
     const [image, setImage] = useState('');
-    const [firstName, setFirstName] = useState( isEdit? contacts[id].firstName : '');
+    const [firstName, setFirstName] = useState(isEdit ? contacts[id].firstName : '');
     const [secondName, setSecondName] = useState(isEdit ? contacts[id].secondName : '');
     const [lastName, setSurname] = useState(isEdit ? contacts[id].lastName : '');
-    const [numbers, setNumbers] = useState(isEdit ? (contacts[id].telNumbers) : ([{number: '', category: '',}]));
-    const [deletedNumber, setDeletedNumber] = useState({index: -1, delNumber: {number: '', category: ''}});
-    const [emails, setEmails] = useState(isEdit ? (contacts[id].emails) : ([{email: '', category: '',}]));
-    const [deletedEmail, setDeletedEmail] = useState({index: -1, delEmail: {email: '', category: ''}});
-    const [snackbar, setSnackbar] = useState({isVisible: false, message: '', isActionVisible: false, label: ''});
+    const [numbers, setNumbers] = useState(isEdit ? (contacts[id].telNumbers) : ([{ number: '', category: '' }]));
+    const [deletedNumber, setDeletedNumber] = useState({ index: -1, delNumber: { number: '', category: '' } });
+    const [emails, setEmails] = useState(isEdit ? (contacts[id].emails) : ([{ email: '', category: '' }]));
+    const [deletedEmail, setDeletedEmail] = useState({ index: -1, delEmail: { email: '', category: '' } });
+    const [snackbar, setSnackbar] = useState({ isVisible: false, message: '', isActionVisible: false, label: '' });
     const [isDeleteClicked, setIsDeleteClicked] = useState(false); //Logika pomagająca przy procesie usuwania/cofania usunięcia,
     const buildContactObject = (): contactT => {
         return {
@@ -38,55 +40,65 @@ const AddEditScreen  = ({route, navigation}): JSX.Element => {
         };
     };
     const contact = buildContactObject();
-    const onGroups = (): void => {navigate('Groups', {id: id})};
-    const onChangeName = (value: string): void => {setFirstName(value)};
-    const onChangeSecondName = (value: string): void => {setSecondName(value)};
-    const onChangeLastName = (value: string): void => {setSurname(value)};
-    const onDismissSnackbar = (): void => {setSnackbar({isVisible: false, message: '', isActionVisible: false, label: ''})};
-    const onShowSnackbar = (isVisible: true, message: string, isActionVisible: boolean, label: string): void =>
-    {setSnackbar({isVisible: true, message: message, isActionVisible: isActionVisible, label: label})};
+    const onGroups = (): void => {
+        navigate('Groups', { id: id });
+    };
+    const onChangeName = (value: string): void => {
+        setFirstName(value);
+    };
+    const onChangeSecondName = (value: string): void => {
+        setSecondName(value);
+    };
+    const onChangeLastName = (value: string): void => {
+        setSurname(value);
+    };
+    const onDismissSnackbar = (): void => {
+        setSnackbar({ isVisible: false, message: '', isActionVisible: false, label: '' });
+    };
+    const onShowSnackbar = (isVisible: true, message: string, isActionVisible: boolean, label: string): void => {
+        setSnackbar({ isVisible: true, message: message, isActionVisible: isActionVisible, label: label });
+    };
 
     // metody
     const addInputField = (label: string): void => {
-        if(label === formLabels.number){
-            setNumbers([...numbers, {category: '', number: ''}]);
+        if (label === formLabels.number) {
+            setNumbers([...numbers, { category: '', number: '' }]);
         } else if (label === formLabels.email) {
-            setEmails([...emails, {category: '', email: ''}]);
+            setEmails([...emails, { category: '', email: '' }]);
         } else {
             onShowSnackbar(true, 'Something went wrong.', false, '');
-            console.log("Błąd podczas dodawania nowego pola, nieznana etykieta.")
+            console.log('Błąd podczas dodawania nowego pola, nieznana etykieta.');
         }
     };
 
     const onChangeTextInput = (label: string, value: string, index: number): void => {
         onDismissSnackbar();
         let tmpData;
-        if(label === formLabels.number){
+        if (label === formLabels.number) {
             tmpData = [...numbers];
             tmpData[index].number = value;
             setNumbers(tmpData);
-            setDeletedNumber({index, delNumber: tmpData[index]});
+            setDeletedNumber({ index, delNumber: tmpData[index] });
         } else if (label === formLabels.email) {
             tmpData = [...emails];
             tmpData[index].email = value;
             setEmails(tmpData);
-            setDeletedEmail({index, delEmail: tmpData[index]});
-        }
-        else {
+            setDeletedEmail({ index, delEmail: tmpData[index] });
+        } else {
             onShowSnackbar(true, 'Something went wrong.', false, '');
-            console.log("Błąd pdczas zmiany danych w polu tekstowym, nieznana etykieta.")
+            console.log('Błąd pdczas zmiany danych w polu tekstowym, nieznana etykieta.');
         }
     };
 
     const onChangeDropdown = (label: string, value: string, index: number): void => {
-        if(!isDeleteClicked) {
+        if (!isDeleteClicked) {
             onDismissSnackbar();
             let tmpData;
-            if(label === formLabels.number){
+            if (label === formLabels.number) {
                 tmpData = [...numbers];
                 tmpData[index].category = value;
                 setNumbers(tmpData);
-                setDeletedNumber({index, delNumber: tmpData[index]});
+                setDeletedNumber({ index, delNumber: tmpData[index] });
             } else if (label === formLabels.email) {
                 tmpData = [...emails];
                 tmpData[index].category = value;
@@ -103,32 +115,32 @@ const AddEditScreen  = ({route, navigation}): JSX.Element => {
     const onDeleteTextInput = (label: string, index: number): void => {
         setIsDeleteClicked(true);
         let tmpData;
-        if(label === formLabels.number) {
-            setDeletedNumber({index, delNumber: numbers[index]});
+        if (label === formLabels.number) {
+            setDeletedNumber({ index, delNumber: numbers[index] });
             tmpData = [...numbers];
             tmpData.splice(index, 1);
             setNumbers(tmpData);
-            onShowSnackbar(true,'Number deleted.', true, label);
+            onShowSnackbar(true, 'Number deleted.', true, label);
         } else if (label === formLabels.email) {
-            setDeletedEmail({index, delEmail: emails[index]});
+            setDeletedEmail({ index, delEmail: emails[index] });
             tmpData = [...emails];
             tmpData.splice(index, 1);
             setEmails(tmpData);
-            onShowSnackbar(true,'Email deleted.', true, label);
+            onShowSnackbar(true, 'Email deleted.', true, label);
         } else {
             onShowSnackbar(true, 'Something went wrong.', false, '');
-            console.log("Błąd podczas usuwania pola tekstowego, nieznana etykieta.");
+            console.log('Błąd podczas usuwania pola tekstowego, nieznana etykieta.');
         }
     };
 
     const onUndoPressed = (label: string): void => {
         setIsDeleteClicked(false);
         let tmpData;
-        if(label === formLabels.number){
+        if (label === formLabels.number) {
             tmpData = [...numbers];
             tmpData.splice(deletedNumber.index, 0, deletedNumber.delNumber);
             setNumbers(tmpData);
-        } else if(label === formLabels.email){
+        } else if (label === formLabels.email) {
             tmpData = [...emails];
             tmpData.splice(deletedEmail.index, 0, deletedEmail.delEmail);
             setEmails(tmpData);
@@ -140,30 +152,68 @@ const AddEditScreen  = ({route, navigation}): JSX.Element => {
     const onSaveContact = (): void => {
         // TODO: walidacja danych
         // TODO: wyświetlanie grup należących do kontaktu
-        if(mode === modes.create) {
+        if (mode === modes.create) {
             dispatch(createContact(buildContactObject()));
         } else if (mode === modes.edit && id !== null) {
             dispatch(updateContact(buildContactObject(), id));
         }
-        onShowSnackbar(true,'Contact saved.', false, '');
+        onShowSnackbar(true, 'Contact saved.', false, '');
     };
+
+    const checkCameraPermissions = useCallback(async (): Promise<boolean> => {
+        const cameraPermissions = await ImagePicker.getCameraPermissionsAsync();
+        if (cameraPermissions.status === 'granted') {
+            return true;
+        } else {
+            const { status } = await ImagePicker.requestCameraPermissionsAsync();
+            if (status === 'granted') {
+                return true;
+            }
+        }
+        return false;
+    }, []);
+
+    const checkCameraRollPermissions = useCallback(async (): Promise<boolean> => {
+        const cameraRollPermissions = await ImagePicker.getCameraRollPermissionsAsync();
+        if (cameraRollPermissions.status === 'granted') {
+            return true;
+        } else {
+            const { status } = await ImagePicker.requestCameraRollPermissionsAsync();
+            if (status === 'granted') {
+                return true;
+            }
+        }
+        return false;
+    }, []);
 
     const pickImage = useCallback(async (): Promise<void> => {
         try {
-            const result = await ImagePicker.launchImageLibraryAsync({
-                mediaTypes: ImagePicker.MediaTypeOptions.All,
-                allowsEditing: true,
-                aspect: [1, 1],
-                quality: 1,
-            });
-            if (!result.cancelled) {
-                setImage(result.uri);
+            const permission = await checkCameraPermissions() && await checkCameraRollPermissions();
+            if (permission) {
+                const result = await ImagePicker.launchImageLibraryAsync({
+                    mediaTypes: ImagePicker.MediaTypeOptions.Images,
+                    allowsEditing: true,
+                    aspect: [1, 1],
+                    quality: 1,
+                });
+                if (!result.cancelled) {
+                    setImage(result.uri);
+                }
+                console.log(result);
+            } else {
+                Alert.alert(
+                    'Permissions',
+                    'We need camera and camera roll permissions in order to change avatar.',
+                    [
+                        { text: 'OK' },
+                    ],
+                    { cancelable: true },
+                );
             }
-            console.log(result);
         } catch (error) {
             console.log(error);
         }
-    },[]);
+    }, [checkCameraPermissions, checkCameraRollPermissions]);
 
     return (
         <AddEdit
