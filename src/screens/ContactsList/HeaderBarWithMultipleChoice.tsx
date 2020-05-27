eb11850d28da7a3b09e623f2455a6c12e99909eb
Bartłomiej Szlachta <bartlomiej.szlachta@softiq.pl>
2020-05-28 00:28:07 +0200

    Nowy styl nowego headera

--- src/screens/ContactsList/HeaderBarWithMultipleChoice.tsx
+++ src/screens/ContactsList/HeaderBarWithMultipleChoice.tsx
@@ -1,9 +1,12 @@
 import React, { ReactElement } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { colors, padding } from '../../styles/common';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { colors, fonts, padding } from '../../styles/common';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 interface Props {
     elementsCount: number;
+    onClearSelection: () => void;
+    onDeleteContacts: () => void;
 }
 
 const styles = StyleSheet.create({
@@ -11,15 +14,33 @@ const styles = StyleSheet.create({
         height: 56,
         backgroundColor: colors.primaryDark,
         padding: 5,
-        paddingHorizontal: padding.sm,
+        paddingHorizontal: padding.md,
         justifyContent: 'center',
+        flexDirection: 'row',
+    },
+    button: {
+        justifyContent: 'center',
+    },
+    text: {
+        flex: 1,
+        paddingHorizontal: padding.md,
+        textAlignVertical: 'center',
+        fontSize: fonts.md,
     },
 });
 
 const HeaderBarWithMultipleChoice = (props: Props): ReactElement => {
+    const { elementsCount, onClearSelection, onDeleteContacts } = props;
+
     return (
         <View style={styles.container}>
-            <Text>{props.elementsCount} item(s) selected</Text>
+            <TouchableOpacity style={styles.button} onPress={onClearSelection}>
+                <Icon name='md-close' size={25} />
+            </TouchableOpacity>
+            <Text style={styles.text}>{elementsCount} item(s) selected</Text>
+            <TouchableOpacity style={styles.button} onPress={onDeleteContacts}>
+                <Icon name='md-trash' size={25} />
+            </TouchableOpacity>
         </View>
     );
 };
