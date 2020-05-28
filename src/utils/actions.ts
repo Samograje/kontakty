d1004022e836663c005eaa2eea8091dc71d5ec72
oliwiabrozek <oliwiabrozek@gmail.com>
2020-05-28 16:40:52 +0200

    Naprawienie dzwonienia

--- src/utils/actions.ts
+++ src/utils/actions.ts
@@ -1,20 +1,18 @@
 import { Linking, Platform } from 'react-native';
 
-export const makeCall = (phoneNumber: string): void => {
-    if (phoneNumber) {
-        if (Platform.OS === 'android') {
-            phoneNumber = 'tel:${phoneNumber}';
-        } else {
-            phoneNumber = 'telprompt:${phoneNumber}';
-        }
-        Linking.openURL(phoneNumber)
-            .then((r) => console.log(r))
-            .catch((error) => {
-                console.log('There has been a problem with operation: ' + error.message);
-                // ADD THIS THROW error
-                throw error;
-            });
+export const makeCall = (phoneNumber: string): Promise<any> => {
+    if (Platform.OS === 'android') {
+        phoneNumber = `tel:${phoneNumber}`;
+    } else {
+        phoneNumber = `telprompt:${phoneNumber}`;
     }
+    return Linking.openURL(phoneNumber)
+        .then((r) => console.log(r))
+        .catch((error) => {
+            console.log('There has been a problem with operation: ' + error.message);
+            // ADD THIS THROW error
+            throw error;
+        });
 };
 
 export const sendSMS = (phoneNumber: string): void => {
